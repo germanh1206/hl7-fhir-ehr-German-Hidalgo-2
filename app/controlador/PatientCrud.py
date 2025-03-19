a3c30ebcc4b9fefe437873dd7e7ad8c2169354f8
germanh1206 <83923450+germanh1206@users.noreply.github.com>
2025-03-19 16:51:40 -0500

    Update PatientCrud.py

--- app/controlador/PatientCrud.py
+++ app/controlador/PatientCrud.py
@@ -3,7 +3,7 @@ from bson import ObjectId
 from fhir.resources.patient import Patient
 import json
 
-collection = connect_to_mongodb("SamplePatient", "patients")
+collection = connect_to_mongodb("patientsRIS", "patients")
 
 def GetPatientById(patient_id: str):
     try:
