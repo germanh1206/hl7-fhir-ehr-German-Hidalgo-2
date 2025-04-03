a50c264e71b09e2b126dc69f996e02c411950fd4
germanh1206 <83923450+germanh1206@users.noreply.github.com>
2025-04-03 16:38:52 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -24,16 +24,6 @@ async def get_patient_by_id(patient_id: str):
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
 
-@app.get("/patients/search", response_model=dict)
-async def get_patient_by_cc(system: str, value: str):
-    status, patient = GetPatientByCC(system, value)
-    if status == "success":
-        return patient
-    elif status == "notFound":
-        raise HTTPException(status_code=404, detail="Patient not found")
-    else:
-        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
-
 
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
